d9054f5ba634ccbdf3d10a2caedc9ab88b2509ca
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2021-02-03 10:02:07 +0800

    First Commit + Menu

---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,33 @@
+{{-- Home --}}
+<li class="nav-item"><a class="d-flex align-items-center" href="/"><i data-feather="home"></i><span class="menu-title text-truncate">Home</span></a></li>
+
+{{-- Data Master--}}
+<li class="navigation-header"><span>Master</span><i data-feather="more-horizontal"></i></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="user"></i><span class="menu-title text-truncate">User</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="user-check"></i><span class="menu-title text-truncate">Reviewer</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="user-plus"></i><span class="menu-title text-truncate">Author</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="flag"></i><span class="menu-title text-truncate">Nation</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="help-circle"></i><span class="menu-title text-truncate">FAQ</span></a></li>
+
+{{-- Data Conference --}}
+<li class="navigation-header"><span>Conference</span><i data-feather="more-horizontal"></i></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="cloud"></i><span class="menu-title text-truncate">Conference</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="activity"></i><span class="menu-title text-truncate">Topik</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="key"></i><span class="menu-title text-truncate">Keynote Speaker</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="calendar"></i><span class="menu-title text-truncate">Important Date</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="info"></i><span class="menu-title text-truncate">Announcement</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="dollar-sign"></i><span class="menu-title text-truncate">Sponsor</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="credit-card"></i><span class="menu-title text-truncate">Voucher</span></a></li>
+
+{{-- Data Submission --}}
+<li class="navigation-header"><span>Submission</span><i data-feather="more-horizontal"></i></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="file-text"></i><span class="menu-title text-truncate">Submission</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="users"></i><span class="menu-title text-truncate">Pembagian Review</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="book-open"></i><span class="menu-title text-truncate">Book of Abstract</span></a></li>
+
+{{-- Data Laporan --}}
+<li class="navigation-header"><span>Laporan</span><i data-feather="more-horizontal"></i></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="users"></i><span class="menu-title text-truncate">Peserta Conference</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="file-text"></i><span class="menu-title text-truncate">Submission</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="shopping-bag"></i><span class="menu-title text-truncate">Pembayaran Author</span></a></li>
+<li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="book"></i><span class="menu-title text-truncate">Published Article</span></a></li>
